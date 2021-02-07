c2c5917441f86a36586e53807428cafce69b7859
TeamTitans449 <75844454+TeamTitans449@users.noreply.github.com>
2021-02-07 20:17:04 +0530

    trial
    trial

---
+++ resp.js
@@ -0,0 +1,6 @@
+burger = document.querySelector('.burger')
+nav = document.querySelector('.nav ')
+// burger = document.querySelector('.burger')
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('v-class')
+})
